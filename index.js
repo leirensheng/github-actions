a50bf337b2ab804e91c7c47c1e1de466633a3b42
leirensheng <leirensheng@163.com>
2022-05-25 15:36:47 +0800

    feat: test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let puppeteer = require("puppeteer-core");
 let start = require("main")
+(async () => {
+  browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
+    headless: false,
+  });
+  const page = await browser.newPage(); //打开一个空白页
 
-browser = await puppeteer.launch({
-  args: ["--no-sandbox"],
-  executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
-  headless: false,
-});
-const page = await browser.newPage(); //打开一个空白页
-
-await start(page);
-await browser.close();
+  await start(page);
+  await browser.close();
+})();
