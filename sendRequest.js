02cfbd37756d50fc73fcc62ebff9e96d3a4fb25c
leirensheng <leirensheng@163.com>
2022-05-31 16:10:25 +0800

    feat: test

---
+++ sendRequest.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+let content = fs.readFileSync("./responseData", "utf-8");
+let body = {
+  token: "ff7273be19b84a01b99f47cedbfb8694",
+  title: "BANK",
+  content: content,
+  template: "html",
+  channel: "wechat",
+};
+fetch("https://www.pushplus.plus/api/send", {
+  body,
+  method: "POST",
+});
