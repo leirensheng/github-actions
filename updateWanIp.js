7926ebbaad3133a068b4c8914cdaf64c3d66fe88
leirensheng <leirensheng@163.com>
2022-09-30 17:28:24 +0800

    feat: update

--- updateWanIp.js
+++ updateWanIp.js
@@ -2,36 +2,36 @@ let startBrowser = require("./startBrowser");
 const { sleep } = require("./utils");
 
 let start = async (page) => {
+  let myClick = async (selector) => {
+    await page.waitForSelector(selector);
+    await page.evaluate((selector) => {
+      document.querySelector(selector).click();
+    },selector);
+  };
   await page.goto("http://leirensheng.dynv6.net:2048/index.html");
   let needLogin = await page.evaluate(() => location.href.includes("login"));
   if (needLogin) {
     console.log("需要登录");
-    await sleep(1000)
+    await sleep(1000);
     await page.evaluate(() => {
       document.querySelector("input[type=password]").value = "07505461203";
       document.querySelector("#save").click();
     });
-    console.log("点击了登录")
+    console.log("点击了登录");
     await page.waitForNavigation();
   }
-  await sleep(1000)
-  await page.click("#system");
-  console.log("点击了系统菜单")
-
-
-  await page.waitForSelector("#reboot");
+  await myClick("#system");
+  console.log("点击了系统菜单");
 
   page.on("dialog", async (dialog) => {
     console.log(dialog.message());
     await dialog.accept();
-    console.log("点击弹窗确定了")
+    console.log("点击弹窗确定了");
     // await browser.close();
   });
-  await sleep(1000)
-  await page.evaluate(() => {
-    document.querySelector("#reboot").click();
-  });
+
+  await myClick("#reboot")
   console.log("点击了重启")
-  await sleep(1000)
+  await sleep(1000);
 };
 startBrowser(start);
